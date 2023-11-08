6a971186d913c21534485228c6d099ca717bcc0b
Konbraphat <101827492+konbraphat51@users.noreply.github.com>
2023-11-08 16:00:16 +0900

    Add: Button id

--- src/components/Initializer/Initializer.vue
+++ src/components/Initializer/Initializer.vue
@@ -1,14 +1,21 @@
 <template>
     <div id="Initializer">
-        a
+        <NGWordSetter />
+        <TextSetter />
+
+        <button id="PlayButton">Play</button>
     </div>
 </template>
 
 <script>
+import NGWordSetter from './NGWordSetter/NGWordSetter.vue';
+
     export default Vue.defineComponent({
         name: 'Initializer',
         components: {
-            //"COMPONENT": Vue.defineAsyncComponent(() => loadModule("PATH.vue", options))
-        },
+    "TextSetter": Vue.defineAsyncComponent(() => loadModule("src/components/Initializer/TextSetter/TextSetter.vue", options)),
+    "NGWordSetter": Vue.defineAsyncComponent(() => loadModule("src/components/Initializer/NGWordSetter/NGWordSetter.vue", options)),
+    NGWordSetter
+},
     })
 </script>
---
+++ src/components/Initializer/NGWordSetter/NGWordSetter.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="NGWordSetter">
+        NGWordSetter
+    </div>
+</template>
+
+<script>
+    export default Vue.defineComponent({
+        name: 'NGWordSetter',
+        components: {
+            //"COMPONENT": Vue.defineAsyncComponent(() => loadModule("PATH.vue", options))
+        },
+    })
+</script>

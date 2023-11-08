32eca6e741dab353692540295a3b3ebbb6b3cacf
Konbraphat <101827492+konbraphat51@users.noreply.github.com>
2023-11-08 16:08:48 +0900

    Refac: unneed import

--- src/components/Initializer/Initializer.vue
+++ src/components/Initializer/Initializer.vue
@@ -2,20 +2,17 @@
     <div id="Initializer">
         <NGWordSetter />
         <TextSetter />
-        
+
         <button id="PlayButton">Play</button>
     </div>
 </template>
 
 <script>
-    import NGWordSetter from './NGWordSetter/NGWordSetter.vue';
-
     export default Vue.defineComponent({
         name: 'Initializer',
         components: {
             "TextSetter": Vue.defineAsyncComponent(() => loadModule("src/components/Initializer/TextSetter/TextSetter.vue", options)),
-            "NGWordSetter": Vue.defineAsyncComponent(() => loadModule("src/components/Initializer/NGWordSetter/NGWordSetter.vue", options)),
-            NGWordSetter
+            "NGWordSetter": Vue.defineAsyncComponent(() => loadModule("src/components/Initializer/NGWordSetter/NGWordSetter.vue", options))
         },
     })
 </script>

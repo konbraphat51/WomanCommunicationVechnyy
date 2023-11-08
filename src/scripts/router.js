a52dde9a97fe494677ae82da3e90a4664a6c489b
Konbraphat <101827492+konbraphat51@users.noreply.github.com>
2023-11-08 11:08:47 +0900

    Test: Use hash mode

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,23 +1,11 @@
-const routes = [
-    {
-        path: "/WomanCommunicationVechnyy",
-        component: Vue.defineAsyncComponent(() => loadModule("src/components/EmptyRouter.vue", options)),
-        children:[
-            {
-                path: "/",
-                name: "Initializer",
-                component: Vue.defineAsyncComponent(() => loadModule("src/components/Initializer/Initializer.vue", options))
-            },
-            // {
-            //     path: "/ingame",
-            //     name: "InGame",
-            //     component: Vue.defineAsyncComponent(() => loadModule("src/components/InGame/InGame.vue", options))
-            // }
-        ]
-    }
-]
-
 const router = VueRouter.createRouter({
+    mode:"hash",
     history: VueRouter.createWebHistory(),
-    routes: routes
+    routes: [
+        {
+            path: "/",
+            name: "Initializer",
+            component: Vue.defineAsyncComponent(() => loadModule("src/components/Initializer/Initializer.vue", options))    //Initializer
+        }
+    ]
 })

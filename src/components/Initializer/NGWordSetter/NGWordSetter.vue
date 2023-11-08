42202ef6e945f36a66f6376875561d9687e0e466
Konbraphat <101827492+konbraphat51@users.noreply.github.com>
2023-11-08 17:15:46 +0900

    Add: Read ng words by input field

--- src/components/Initializer/NGWordSetter/NGWordSetter.vue
+++ src/components/Initializer/NGWordSetter/NGWordSetter.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="NGWordSetter">
-        NGWordSetter
+        <div class="DropZone" @drop="ReadFile">
+            <p>Drop here</p>
+            <input type="file" @change="ReadFileByInput"/>
+        </div>
     </div>
 </template>
 
@@ -20,7 +23,29 @@
                 return {
                     words: this.words
                 }
+            },
+            ReadFileByInput(event){
+                const file = event.target.files[0]; // 選択されたファイル
+                if (file) {
+                    let _this = this
+                    const reader = new FileReader();
+                    reader.onload = function (e) {
+                        const content = e.target.result; // ファイルの内容
+                        const lines = content.split(/\r\n|\r|\n/); // 改行で分割
+                        _this.words = lines; // 改行区切りの文字列の配列
+                        console.log(_this.words)
+                    };
+                    reader.readAsText(file); // ファイルの内容をテキストとして読み込む
+                }
             }
         }
     })
 </script>
+
+<style>
+    .DropZone {
+        border: 5px solid blue;
+        width: 200px;
+        height: 100px;
+    }
+</style>

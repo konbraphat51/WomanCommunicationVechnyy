ef42a15b2106aa38c11472bf6618845232b22b98
Konbraphat <101827492+konbraphat51@users.noreply.github.com>
2023-11-08 12:52:09 +0900

    Add: [Initializer] Basic structure

---
+++ src/components/Initializer/TextSetter/TextSetter.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="TextSetter">
+        TextSetter
+    </div>
+</template>
+
+<script>
+    export default Vue.defineComponent({
+        name: 'TextSetter',
+        components: {
+            //"COMPONENT": Vue.defineAsyncComponent(() => loadModule("PATH.vue", options))
+        },
+    })
+</script>
